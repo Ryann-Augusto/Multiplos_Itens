5796a2a0f8c9bd0301bba180b142847c5aa8f4e4
Ryann Barbosa <fiscobrasil2018@gmail.com>
2022-09-19 17:47:51 -0300

    Adicionando botao remover input e soma total

--- Multiplos_Itens/wwwroot/js/site.js
+++ Multiplos_Itens/wwwroot/js/site.js
@@ -7,10 +7,11 @@ function adicionarCampo() {
     document.querySelector("#moreItens").insertAdjacentHTML('beforeend',
         ' <div class="form-group mt-2">'
         + '<label class="control-label" for="Produtos_' + controleCampo + '__Nome">Nome</label>'
-        + '<input class="form-control field" type="text" data-val="true" data-val-required="The Nome field is required." id="Produtos_' + controleCampo + '__Nome" name="Produtos[' + controleCampo + '].Nome" value="" onblur="validaForm()">'
+        + '<input class="form-control field" type="text" data-val="true" data-val-required="The Nome field is required." id="Produtos_' + controleCampo + '__Nome" name="Produtos[' + controleCampo + '].Nome" value="" onblur="validaForm()"> </input><a class="btn btn-primary" onclick="removerCampo(' + controleCampo +')"> - </a><br></div>'
         + '<span class="text-danger" id="valida_nome' + controleCampo + '"></span>'
         + '</div>')
     controleCampo++;
+    validaForm();
 }
 
 function validaForm() {
@@ -24,6 +25,7 @@ function validaForm() {
         } else {
             input.removeClass('--has-error')
         }
+        //var valid = positionList.some()
         console.log(positionList)
     }
 }
@@ -35,5 +37,24 @@ function null_or_empty(str, contador) {
     }
     else {
         return "Com valores" + contador;
+    }
+}
+
+function removerCampo(idCampo) {
+    document.querySelector("#campo" + idCampo).remove();
+}
+
+
+function valorTotal(idCampo) {
+
+    var valor1 = document.querySelector("#valor" + idCampo).value;
+    var quantidade = document.querySelector("#quantidade" + idCampo).value;
+
+    if (valor1 == 0 || quantidade == 0) {
+        document.querySelector("#total" + idCampo).innerHTML = 0;
+    }
+    else {
+        var total = parseFloat(valor1) * parseInt(quantidade);
+        document.querySelector("#total" + idCampo).value = parseFloat(total);
     }
 }
